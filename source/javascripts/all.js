c5c8592590010fa736d3911a03b9cd3f4b6d9e7f
kyktommy <kyktommy@gmail.com>
2013-11-11 17:47:57 +0800

    added isotope.js to filter different tag

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,23 @@
 //= require_tree .
+
+$(function() {
+
+  var $itemList = $('#item-list');
+  $itemList.isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows',
+    masonry: { columnWidth : 230 }
+  });
+
+  $('#tag-list li').each(function(index, item) {
+    $(item).bind('click', function() {
+      $this = $(this);
+      $this.addClass('active');
+      $this.siblings().removeClass('active');
+      
+      var filter = $this.data('filter');
+      $itemList.isotope({filter: filter});
+    });
+  });
+
+});
